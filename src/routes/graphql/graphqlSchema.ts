6d99e8a62864631beae6f1784f9778f2cf176a98
LinderJK <slepchenkovia@gmail.com>
2024-10-31 02:15:29 +0300

    refactor: change types, add nonnull

--- src/routes/graphql/graphqlSchema.ts
+++ src/routes/graphql/graphqlSchema.ts
@@ -1,36 +1,54 @@
 import {
+  GraphQLEnumType,
   GraphQLFloat,
   GraphQLInt,
   GraphQLList,
+  GraphQLNonNull,
   GraphQLObjectType,
   GraphQLSchema,
   GraphQLString,
 } from 'graphql/index.js';
 
-const MemberType = new GraphQLObjectType({
-  name: 'MemberType',
-  fields: {
-    id: { type: GraphQLString },
-    discount: { type: GraphQLFloat },
-    postsLimitPerMonth: { type: GraphQLInt },
+const MemberTypeId = new GraphQLEnumType({
+  name: 'MemberTypeId',
+  values: {
+    BASIC: { value: 'BASIC' },
+    BUSINESS: { value: 'BUSINESS' },
   },
 });
 
-const
+const MemberType = new GraphQLObjectType({
+  name: 'MemberType',
+  fields: {
+    id: { type: new GraphQLNonNull(MemberTypeId) },
+    discount: { type: new GraphQLNonNull(GraphQLFloat) },
+    postsLimitPerMonth: { type: new GraphQLNonNull(GraphQLInt) },
+  },
+});
+
+// const Post = new GraphQLObjectType({
+//   name: 'Post',
+//   fields: {
+//     id: { type: GraphQLString },
+//     title: { type: GraphQLString },
+//     content: { type: GraphQLString },
+//   },
+// });
+/////////////////////////////////////////////////////////////////////////
 
 const RootQuery = new GraphQLObjectType({
-  name: 'RootQueryType',
+  name: 'RootQuery',
   fields: {
     memberTypes: {
-      type: new GraphQLList(MemberType),
+      type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(MemberType))),
       resolve: async (_, __, { prisma }) => {
         return await prisma.memberType.findMany();
       },
     },
-    memberTypeId: {
+    memberType: {
       type: MemberType,
       args: {
-        id: { type: GraphQLString },
+        id: { type: new GraphQLNonNull(MemberTypeId) },
       },
       resolve: async (_, args, { prisma }) => {
         return await prisma.memberType.findUnique({
