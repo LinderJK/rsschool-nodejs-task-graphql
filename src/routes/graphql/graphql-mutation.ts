9e38998bd29317634cc904910b5fa00fea7a6679
LinderJK <slepchenkovia@gmail.com>
2024-11-03 20:24:09 +0300

    feat: add create for post, profile, add change for post, profile, user

--- src/routes/graphql/graphql-mutation.ts
+++ src/routes/graphql/graphql-mutation.ts
@@ -1,6 +1,21 @@
-import { GraphQLNonNull, GraphQLObjectType } from 'graphql/index.js';
-import { userType } from './graphql-types.js';
-import { CreateUserInputType } from './graphql-inputs.js';
+import {
+  GraphQLBoolean,
+  GraphQLFloat,
+  GraphQLInt,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLString,
+} from 'graphql/index.js';
+import { MemberTypeId, postType, profileType, userType } from './graphql-types.js';
+import { UUIDType } from './types/uuid.js';
+import {
+  ChangePostInputType,
+  ChangeProfileInputType,
+  ChangeUserInputType,
+  CreatePostInputType,
+  CreateProfileInputType,
+  CreateUserInputType,
+} from './graphql-inputs.js';
 
 const graphqlMutations = new GraphQLObjectType({
   name: 'Mutation',
@@ -12,6 +27,71 @@ const graphqlMutations = new GraphQLObjectType({
       },
       resolve: async (_, { dto }, { prisma }) => {
         return await prisma.user.create({
+          data: dto,
+        });
+      },
+    },
+    createPost: {
+      type: postType,
+
+      args: {
+        dto: { type: new GraphQLNonNull(CreatePostInputType) },
+      },
+
+      resolve: async (_, { dto }, { prisma }) => {
+        return await prisma.post.create({
+          data: dto,
+        });
+      },
+    },
+    createProfile: {
+      type: profileType,
+
+      args: {
+        dto: { type: new GraphQLNonNull(CreateProfileInputType) },
+      },
+
+      resolve: async (_, { dto }, { prisma }) => {
+        return await prisma.profile.create({
+          data: dto,
+        });
+      },
+    },
+    changePost: {
+      type: postType,
+      args: {
+        id: { type: new GraphQLNonNull(UUIDType) },
+        dto: { type: new GraphQLNonNull(ChangePostInputType) },
+      },
+      resolve: async (_, { id, dto }, { prisma }) => {
+        return await prisma.post.update({
+          where: { id },
+          data: dto,
+        });
+      },
+    },
+    changeProfile: {
+      type: profileType,
+      args: {
+        id: { type: new GraphQLNonNull(UUIDType) },
+        dto: { type: new GraphQLNonNull(ChangeProfileInputType) },
+      },
+      resolve: async (_, { id, dto }, { prisma }) => {
+        return await prisma.profile.update({
+          where: { id },
+          data: dto,
+        });
+      },
+    },
+    changeUser: {
+      type: userType,
+      args: {
+        id: { type: new GraphQLNonNull(UUIDType) },
+        dto: { type: new GraphQLNonNull(ChangeUserInputType) },
+      },
+      resolve: async (_, { id, dto }, { prisma }) => {
+        return await prisma.user.update({
+          where: { id },
           data: dto,
         });
       },
