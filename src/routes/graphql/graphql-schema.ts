ea46ec308996e3cd59c629ba4cdcca7ecdbde504
LinderJK <slepchenkovia@gmail.com>
2024-11-03 20:20:48 +0300

    feat: add mutation

--- src/routes/graphql/graphql-schema.ts
+++ src/routes/graphql/graphql-schema.ts
@@ -1,8 +1,10 @@
 import { GraphQLSchema } from 'graphql';
 import RootQuery from './graphql-query.js';
+import graphqlMutations from './graphql-mutation.js';
 
 const RootSchema = new GraphQLSchema({
   query: RootQuery,
+  mutation: graphqlMutations,
 });
 
 export default RootSchema;
